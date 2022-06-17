42e58e8c25bb2e4eb68f65f330073863c89ec527
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-06-17 23:44:23 +0200

    estructura de rutas y controladores básica

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,20 +4,24 @@ const getAllTasks = (req, res) => {
     res.send("All task from the controller")
 }
 const createNewTask = (req, res) => {
+
     console.log(req.body)
     res.send("Create new task")
 }
 const getSingleTask = (req, res) => {
+    const idParam = req.params.id
+
     console.log(req.params)
-    res.send("Get single quote")
+    res.send("Get single quote " + idParam)
 }
 const updateTask = (req, res) => {
-    console.log(req.params)
+    const idParam = req.params.id
     console.log(req.body)
-    res.send("Update a task")
+    res.send("Update a task " + idParam)
 }
 const deleteTask = (req, res) => {
-    console.log(req.params)
-    res.send("Delete task")
+    const idParam = req.params.id
+
+    res.send("Delete task " + idParam)
 }
 export { getAllTasks, createNewTask, getSingleTask, updateTask, deleteTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getAllTasks, getSingleTask } from '../controllers/tasks.js'
+import { getAllTasks, getSingleTask, createNewTask, deleteTask, updateTask } from '../controllers/tasks.js'
 const tasksRouter = Router()
 
 tasksRouter.route('/').get(getAllTasks)
+tasksRouter.route('/').post(createNewTask)
 tasksRouter.route('/:id').get(getSingleTask)
+tasksRouter.route('/:id').patch(updateTask)
+tasksRouter.route('/:id').delete(deleteTask)
 
 export default tasksRouter
