d40a25829f4f954a37852ccc1e31581d8b67f56e
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-06-22 19:38:32 +0200

    read all taks and look for a single task and also delete operation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,31 +1,57 @@
 import taskModel from "../models/task.js"
 
-const getAllTasks = (req, res) => {
-    res.send("All task from the controller")
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await taskModel.find({});
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ "error getting all tasks": error })
+    }
 }
 const createNewTask = async (req, res) => {
     try {
         const taskCreated = await taskModel.create(req.body)
+
         res.status(201).json({ taskCreated })
 
     } catch (error) {
-        res.status(500).json({ "error": error })
+        res.status(500).json({ "error creating a new task": error })
     }
 }
-const getSingleTask = (req, res) => {
+const getSingleTask = async (req, res) => {
     const idParam = req.params.id
-
-    console.log(req.params)
-    res.send("Get single quote " + idParam)
+    try {
+        const taskSearched = await taskModel.findOne({ _id: idParam })
+        console.log(taskSearched)
+        if (!taskSearched) {
+            return res.status(404).json({ msg: `No task with id: ${idParam} was found` })
+        }
+        res.status(200).json(taskSearched)
+    } catch (error) {
+        res.status(500).json({ "error looking for a single task": error })
+    }
 }
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ "error ": error })
+    }
     const idParam = req.params.id
     console.log(req.body)
     res.send("Update a task " + idParam)
 }
-const deleteTask = (req, res) => {
-    const idParam = req.params.id
+const deleteTask = async (req, res) => {
+    try {
+        const idParam = req.params.id
+        const taskDeleted = await taskModel.findOneAndDelete({ _id: idParam })
+        if (!taskDeleted) {
+            return res.status(404).json({ msg: `No task with id: ${idParam} was found` })
+        }
+        res.status(200).json({ taskDeleted })
+    } catch (error) {
+        res.status(500).json({ "error deleting the task": error })
+    }
 
-    res.send("Delete task " + idParam)
 }
 export { getAllTasks, createNewTask, getSingleTask, updateTask, deleteTask };
